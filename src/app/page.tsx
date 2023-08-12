e6e1620399608b730d9cb7558c777c3bda47e764
Kan Ninomiya <ninomiyakan.dev@gmail.com>
2023-08-12 14:39:56 +0900

    top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-      <main>
-        <h1>go5.run</h1>
-      </main>
-  )
+    return (
+        <main className="flex flex-col items-center justify-center h-screen">
+            <Link href={'https://go5.run/'}>
+                <h1 className="text-center">go5.run</h1>
+            </Link>
+            <div id={'detail'} className="text-center">
+                <Link href="https://status.go5.run/">
+                    <div className="cursor-pointer">
+                        👣MyStatus
+                    </div>
+                </Link>
+                <Link href="https://go5.run/csv/">
+                    <div className="cursor-pointer">
+                        📒CSV Viewer
+                    </div>
+                </Link>
+            </div>
+        </main>
+    )
 }
