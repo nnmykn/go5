d9e9a98866c6866da8d5b660999d53092782ea7c
nnmykn <ninomiya@kan.run>
2023-10-14 20:10:16 +0900

    Gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,35 @@
-import Link from "next/link";
+'use client'
+
+import Link from 'next/link';
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center justify-center h-screen">
+        <main className="flex flex-col items-center justify-center h-screen white">
+            <style jsx>{`
+              .rotate {
+                color: transparent;
+                background-image: url('/Gradient-small.png');
+                background-size: 300% 300%;
+                filter: brightness(1.2);
+                background-clip: text;
+                -webkit-background-clip: text;
+                animation: rotation 10s infinite linear alternate;
+              }
+
+              @keyframes rotation {
+                from {
+                  background-position: 0 0;
+                }
+                to {
+                  background-position: 0 100%;
+                }
+              }
+            `}</style>
             <Link href={'https://go5.run/'}>
-                <h1 className="text-center">go5.run</h1>
+                <h1 className={`text-center text-5xl font-extrabold rotate`}>
+                    Go5 Lab.
+                </h1>
             </Link>
-            <div id={'detail'} className="text-center">
-                <Link href="https://status.go5.run/">
-                    <div className="cursor-pointer">
-                        👣MyStatus
-                    </div>
-                </Link>
-                <Link href="https://go5.run/csv/">
-                    <div className="cursor-pointer">
-                        📒CSV Viewer
-                    </div>
-                </Link>
-            </div>
         </main>
-    )
+    );
 }
